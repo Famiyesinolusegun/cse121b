5f18c94514364b9f31b85ca8e8d60a80c8105749
Famiyesinolusegun <114127821+Famiyesinolusegun@users.noreply.github.com>
2024-02-04 06:04:28 +0100

    cse121

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -1,61 +1,60 @@
-/* LESSON 3 - Programming Tasks */
+/* W02-Task - Profile Home Page */
 
-/* Profile Object */
-let myProfile = {
-      name: "Olusegun Famiyesin",
-      photo: "images/profile.png",
-      favoriteFoods: [
-        "Pancake", "Pizza", "Burguer","Bread"
-      ],
-      hobbies: [  'Listening to music','Playing game ','Reading','Coding',],
-      placesLived: [
-        {
-          place: 'Ibadan, OY',
-          length: '4 year'
-        },
-        {
-          place: 'Lagos, LA',
-          length: '6 years'
-        },
-        {
-          place: 'Ilesa, OS',
-          length: '8 years'
-        }
-      ]
-    };
-    
-    /* DOM Manipulation - Output */
-    
-    /* Name */
-    document.querySelector('#name').textContent = myProfile.name;
-    
-    /* Photo with attributes */
-    document.querySelector('#photo').src = myProfile.photo;
-    document.querySelector('#photo').alt = myProfile.name;
-    
-    /* Favorite Foods List */
-    myProfile.favoriteFoods.forEach(food => {
-      let li = document.createElement('li');
-      li.textContent = food;
-      document.querySelector('#favorite-foods').appendChild(li);
-    });
-    
-    /* Hobbies List */
-    myProfile.hobbies.forEach(hobby => {
-      let li = document.createElement('li');
-      li.textContent = hobby;
-      document.querySelector('#hobbies').appendChild(li);
-    });
-    
-    /* Places Lived DataList */
-    myProfile.placesLived.forEach(place => {
-      let dt = document.createElement('dt');
-      dt.textContent = place.place;
-      
-      let dd = document.createElement('dd');
-      dd.textContent = place.length;
-      
-      document.querySelector('#places-lived').appendChild(dt);
-      document.querySelector('#places-lived').appendChild(dd);
-    });
-  
+
+/* Step 2 - Variables */
+const fullName = "Olusegun Famiyesin";
+const  currentYear = '2024';
+const profilePicture = 'images/profile.png';
+
+
+/* Step 3 - Element Variables */
+
+const nameElement = document.getElementById('name');
+const foodElement = document.getElementById('food');
+const yearElement = document.querySelector('#year');
+const imageElement = document.querySelector('img');
+
+
+/* Step 4 - Adding Content */
+nameElement.innerHTML = `<strong>${fullName}</strong>`
+
+window.onload = () => {
+
+    function Foods(foodArray) {
+        foodElement.innerHTML = foodArray.reduce((acc, curr) => {
+            acc += `<br>${curr}`
+            return acc
+        }, '')
+    }
+
+    nameElement.innerHTML = `<strong>${fullName}</strong>`;
+    yearElement.textContent = currentYear;
+    imageElement.setAttribute('src', profilePicture)
+    imageElement.setAttribute('alt', `Profile image of ${fullName}`)
+
+    /* Step 5 - Array */
+
+    const favFoods = []
+
+    favFoods.push('pancake,Pizza,Burguer,bread')
+    Foods(favFoods)
+
+    let newfavFoods = 'rice,beans,ice cream,apples,'
+
+    favFoods.push(newfavFoods)
+    Foods(favFoods) 
+
+    favFoods.pop();
+    Foods(favFoods)
+
+    favFoods.push('pancake,Pizza,Burguer,bread')
+    Foods(favFoods)
+
+    favFoods.shift()
+    Foods(favFoods)
+
+    newfavFoods = 'rice,beans,ice cream,apples,'
+
+    favFoods.push(newfavFoods)
+    Foods(favFoods)
+}
